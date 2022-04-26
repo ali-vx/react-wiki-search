0add1354325fa1078242668d27875827de4bd2da
Muhammad Ali <amuhammad161@gmail.com>
2022-04-27 00:30:02 +0500

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
+import { useState } from "react";
+
 function App() {
+	const [search, setSearch] = useState("");
+	const [results, setResults] = useState([]);
+	const [searchInfo, setSearchInfo] = useState({});
+
+	const handleSearch = async (e) => {
+		e.preventDefault();
+		if (search === "") return;
+
+		const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+		const response = await fetch(endpoint);
+		if (!response.ok) {
+			throw new Error(`Request rejected with status ${response.status}`);
+		}
+		const data = await response.json();
+		setResults(data.query.search);
+		setSearchInfo(data.query.searchinfo);
+	};
+
 	return (
 		<div className="App">
 			<header>
 				<h1>Wiki Search</h1>
-				<form className="search-box">
-					<input type="search" placeholder="Search Wikipedia..." />
+				<form className="search-box" onSubmit={handleSearch}>
+					<input
+						type="search"
+						placeholder="Search Wikipedia..."
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
 				</form>
-				<p>Search Results: 0</p>
+				{searchInfo.totalhits ? (
+					<p>Search Results: {searchInfo.totalhits}</p>
+				) : (
+					""
+				)}
 			</header>
 			<div className="results">
-				<div className="result">
-					<h3>Title</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-						mattis nec felis at laoreet. Nunc vel lacus a dolor commodo
-						facilisis eget et velit. Aenean ut ante magna.
-					</p>
-					<a href="#">Read More</a>
-				</div>
+				{results.map((result, i) => {
+					const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+					return (
+						<div className="result" key={i}>
+							<h3>{result.title}</h3>
+							<p dangerouslySetInnerHTML={{ __html: result.snippet }}></p>
+							<a target="_blank" rel="noopener noreferrer" href={url}>
+								Read More
+							</a>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
